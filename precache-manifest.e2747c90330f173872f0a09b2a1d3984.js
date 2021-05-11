3f400e27fb836cfe307b9f18a20e1a3200d673f6
Arjav Garg <arjavgarg@gmail.com>
2021-05-11 09:46:39 +0530

    build

---
+++ precache-manifest.e2747c90330f173872f0a09b2a1d3984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36385c214a599c34da045146813b789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59dad1ac08f67e75a6a",
+    "url": "/static/css/main.eace5661.chunk.css"
+  },
+  {
+    "revision": "f222c47e0af6ab4b1f1e",
+    "url": "/static/js/2.b908b761.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b908b761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59dad1ac08f67e75a6a",
+    "url": "/static/js/main.73e2c6fc.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- src/Constants.js
+++
@@ -1,7 +0,0 @@
-export const avatar_img='https://avatars3.githubusercontent.com/u/22453292';
-export const about_me_pts =['Undergraduate student of Computer Science and Engineering at BITS Pilani, Hyderabad Campus', 'Hobbyist Programmer', 'Touched upon almost all aspects of development, ranging from Operating Systems to Front End Web development'];
-export const proj_imgs = ['https://i.imgur.com/jo2QYHf.png', 'https://i.imgur.com/qyNNqbS.png', 'https://i.imgur.com/dNWZS1jl.png', 'https://i.imgur.com/6CBqHYRl.png', 'https://i.imgur.com/zflaQYpl.png', 'https://i.imgur.com/b1Akec7l.png'];
-export const proj_names = ['Hunt for the Lost Mojo', 'Immortal Kombat', 'Platformer Game', 'Front End Web Projects', 'Whack A Mole', 'Garg OS'];
-export const proj_tags = [['Python', 'Pygame'], ['Java', 'Networking'], ['Java', 'AWT', 'Swing'], ['HTML', 'CSS', 'JS'], ['Android'], ['C', 'x86 ASM']];
-export const proj_links = ['https://github.com/arjav2002/Hunt-Mojo', 'https://github.com/arjav2002/Immortal-Kombat', 'https://github.com/arjav2002/Platformer-Game', 'https://codepen.io/agprg', 'https://github.com/arjav2002/whack-a-mole', 'https://github.com/arjav2002/Garg-OS'];
-export const button_icons = ['fas fa-th-large', 'fas fa-list'];
